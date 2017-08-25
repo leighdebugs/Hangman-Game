1ab37be795faeab111230f85cf521a2380c2a217
leighkristine <lkroxmob@gmail.com>
2017-08-25 00:53:36 -0700

    Set up HTML, edited JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+$(document).ready(function(game) {
+
+//defining variables
+
+var guessRem = 12;
+var answers = ["hamilton", "schuyler", "wait for it", "and peggy", "just you wait", 
+				"marquis de lafayette", "ten duel commandments", "everything is legal In jersey",
+				"the world turned upside down", "why do you write like you're running out of time",];
+ var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", 
+ 				"R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+
+
+// setup display
+
+answers.forEach(function(round) {
+var guessThis = answers[Math.floor(Math.random() * answers.length)];
+
+})
+
+
+
+
+
+}
